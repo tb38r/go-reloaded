816a3d40eadfd21e1007269ccea5fccde1fb710c
tb38r <tlawal86@hotmail.co.uk>
2021-10-11 19:33:30 +0100

    moved files about

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 )
 
 func readFromFile() {
-	file, err := os.Open("test.txt")
+	args := os.Args[1:]
+	file, err := os.Open(args[0])
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	fileScanner := bufio.NewScanner(file)
+	fileScanner := bufio.NewScanner(file) // Can use bufio.Scanner to implement "print each line" function
 	for fileScanner.Scan() {
 		fmt.Println(fileScanner.Text())
 	}
