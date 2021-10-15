fdf0de75feade110f2a4c2cf7a9054686878f1e7
tb38r <tlawal86@hotmail.co.uk>
2021-10-15 13:41:12 +0100

    main

--- test/main.go
+++ test/main.go
@@ -9,6 +9,34 @@ import (
 	"strings"
 )
 
+func removebrackets(n string) string {
+
+	var opening int
+	var closing int
+
+	for index, char := range n {
+		if char == '(' {
+			opening = index
+		} else if char == ')' {
+			closing = index + 1
+		}
+	}
+
+	toAdd := closing - opening
+
+	var empty string
+
+	for i := 0; i < len(n); i++ {
+		if i == opening {
+			i += toAdd
+		} else {
+			empty += string(n[i])
+			//empty = append(empty, string(n[i]))
+		}
+	}
+	return empty
+}
+
 func reverse(n []string) []string {
 
 	reversed := []string{}
@@ -140,8 +168,9 @@ func main() {
 				}
 				reform := reverse(reversed)
 				rejoined := strings.Join(reform, " ")
-				fmt.Println(rejoined)
-				fmt.Println(strings.TrimLeft(rejoined, "("))
+				final := removebrackets(rejoined)
+				fmt.Println(final)
+
 			}
 		}
 	}
